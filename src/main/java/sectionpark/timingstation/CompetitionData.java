226e97d368c703c7f9473c40a6e2ef753a3f6741
jcejkaGit <jcejka@student.tgm.ac.at>
2023-01-17 08:20:14 +0100

    Initial commit

---
+++ src/main/java/sectionpark/timingstation/CompetitionData.java
@@ -0,0 +1,27 @@
+package sectionpark.timingstation;
+
+import java.util.ArrayList;
+
+public class CompetitionData {
+    private String unitTiming;
+    private ArrayList<Party> party;
+    
+    public CompetitionData() {
+    	this.unitTiming = "hh:mm:ss";
+    	this.party = new ArrayList<Party>();
+    	
+    }
+    public void setUnitTiming(String unitTiming){
+        this.unitTiming = unitTiming;
+    }
+    public String getUnitTiming(){
+        return this.unitTiming;
+    }
+    public void setParty(ArrayList<Party> party){
+        this.party =  party;
+    }
+    
+    public ArrayList<Party> getParty(){
+        return this.party;
+    }
+}
